d904a1f26b415c94c26d4a94ded72b4b0293ce9d
Zhongming <atenliao@gmail.com>
2023-04-03 00:30:33 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
+
+function ChangeColor(){
+    const found = document.getElementsByTagName('div')
+    found[2].style.background = 'red'
+    found[3].style.background = 'blue'
+    found[4].style.background = 'yellow'
+    found[5].style.background = 'green'
 }
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', ChangeColor)
+
+}
+
+
+addingEventListener()
